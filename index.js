aa56028f9354d0db36e39814fd1bf0b6d0a1e595
Pankaj Karamchandani <pankajkaram9@gmail.com>
2021-03-20 16:19:28 +0530

    Auth Flow

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User')
+require('./services/passport')
 
-const app = express()
+const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app)
+
+mongoose.connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send({id: 'Hello buddy'})
-})
-
-
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`App running on post:- ${PORT}`)
 })
